1038ccaed1d820597f4b94234e2b27578c662eae
Bedilbek Khamidov <bedilbek@gmail.com>
2021-10-27 18:10:44 +0500

    Fix Editor visibility

--- com.unity.perception/Editor/AssemblyInfo.cs
+++ com.unity.perception/Editor/AssemblyInfo.cs
@@ -1,4 +1,7 @@
 using System.Runtime.CompilerServices;
 
+#if UNITY_EDITOR
+[assembly: InternalsVisibleTo("Unity.Perception.Editor")]
+#endif
 [assembly: InternalsVisibleTo("Unity.Perception.Editor.Tests")]
 [assembly: InternalsVisibleTo("Unity.Perception.Runtime.Tests")]
